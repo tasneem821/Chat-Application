7524be19c1ff868dc5e6cd0696bf14bb7e760ae8
ali <ali_20220299@fci.helwan.edu.eg>
2025-05-01 18:53:23 +0300

    i have uploded the login compoment and the register compoment

---
+++ reactpro1/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";  
+import Register from "./Register";  
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
